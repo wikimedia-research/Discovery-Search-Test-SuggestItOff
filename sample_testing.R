5977fac48e239f2cc1ed0de00d1e2d78b1fd1fa0
ironholds <ironholds@gmail.com>
2015-08-11 13:27:21 -0400

    add images, finish sample testing

--- sample_testing.R
+++ sample_testing.R
@@ -1,14 +1,44 @@
-library(rgeolocate)
-library(uaparser)
 library(data.table)
+library(ggplot2)
+library(ggthemes)
+options(scipen = 500)
+library(scales)
 
 load("ab_data.RData")
+data <- as.data.table(data)
 
 # Check for no substantial variation in browsers
-data <- as.data.table(data)
-browser_data <- data[, j = list(events = .N), by = c("test_group", "browser")]
-browser_a <- browser_data[test_group == "a"]
-browser_a <- browser_a[order(browser_a$events, decreasing = TRUE)]
+browser_variation <- function(dataset){
+  browser_data <- dataset[, j = list(events = .N), by = c("test_group", "browser")]
+  browser_a <- browser_data[test_group == "a"]
+  browser_a <- browser_a[order(browser_a$events, decreasing = TRUE)]
+  top_10 <- rbind(browser_a[1:10], browser_data[test_group == "b" & browser %in% browser_a$browser[1:10]])
+  return(top_10)
+}
 
+initial_variation <- browser_variation(data)
+ggsave(filename = "pre_cleanup_browser_variation.png",
+       plot = ggplot(initial_variation, aes(x = reorder(browser, events), y = events, fill = factor(test_group))) +
+         geom_bar(stat="identity", position = "dodge") +
+         theme_fivethirtyeight() + scale_x_discrete() + scale_y_continuous() +
+         labs(title = "Top browsers for each test group") + coord_flip())
 
+sanitised_variation <- browser_variation(data[data$ip != "85.13.134.246"])
+ggsave(filename = "post_cleanup_browser_variation.png",
+       plot = ggplot(sanitised_variation, aes(x = reorder(browser, events), y = events, fill = factor(test_group))) +
+         geom_bar(stat="identity", position = "dodge") +
+         theme_fivethirtyeight() + scale_x_discrete() + scale_y_continuous() +
+         labs(title = "Top browsers for each test group, after cleanup") + coord_flip())
+
+# Check for geographic variation
+geographic_variation <- function(dataset){
+  geo_data <- dataset[, j = list(events = .N), by = c("test_group", "country")]
+  geo_a <- geo_data[test_group == "a"]
+  geo_a <- geo_a[order(geo_a$events, decreasing = TRUE)]
+  top_10 <- rbind(geo_a[1:10], geo_data[test_group == "b" & country %in% geo_a$country[1:10]])
+  return(top_10)
+}
+
+sanitised_geo_variation <- geographic_variation(data[ip != "85.13.134.246"])
+data[ip != "85.13.134.246", j = list(events = .N), by = c("test_group","country")]
 data <- data[,j= .N, by = c("test_group", "ip", "project", "user_agent", "results", "source", "country")]
